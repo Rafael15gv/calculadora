01dfb5e82333278e65d1cca8ecd95f829a63672d
Rafael Gonazalez Vasquez <143367099+Rafael15gv@users.noreply.github.com>
2024-09-22 18:21:22 -0500

    Add files via upload
    Adjunte todos los archivos utilizados para la Calculadora

---
+++ Calculadora/app.js
@@ -0,0 +1,56 @@
+let teclaPresionada;
+let Visual = "0";
+
+
+const display = document.getElementById("display");
+const reset = document.getElementById("reset");
+const borrar = document.getElementById("borrar");
+const pulso = document.querySelectorAll(".tecla");
+
+
+display.textContent= Visual;
+
+pulso.forEach(elemento => {
+    elemento.addEventListener('click', () => {
+        const teclaPresionada = elemento.textContent;
+
+        if (teclaPresionada == "=") {
+            try {
+                Visual = eval(Visual);
+                display.textContent = Visual;
+            } catch (error) {
+                display.textContent = "Error!";
+                Visual = "0";
+            }
+            return
+        }
+
+        if (Visual == "0") {
+            Visual = teclaPresionada;
+            display.textContent = Visual;
+            console.log("entramos a if");
+            
+        } else {
+            Visual += teclaPresionada;
+            display.textContent = Visual;
+        }
+    });
+});
+
+reset.addEventListener('click', () => {
+    Visual = "0";
+    display.textContent= Visual;
+});
+
+
+
+borrar.addEventListener('click', () => {
+    Visual = Visual.slice(0,Visual.length-1);
+    if (Visual == "") {
+        Visual = "0";
+        display.textContent= Visual;
+    
+    } else {
+        display.textContent= Visual;
+    }
+});
